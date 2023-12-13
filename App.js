f34d175fc732dbc037df8ca30ec3b6e61669a2cf
Ahmad Mahyudi Bin Muhamad Fitri <ahmad.mahyudi.muhamad@NONSMTP.rhbgroup.com>
2023-12-13 15:18:02 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, Button, Modal, Alert } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './component/Home';
+import Profile from './component/Profile';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'Welcome Home'}}
+        />
+        <Stack.Screen 
+          name="Profile" 
+          component={Profile} 
+          options={{title: 'Profile'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ component/Home.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, Button, Modal, Alert, SafeAreaView } from 'react-native';
+
+export default function Home({navigation}) {
+  const [isModalVisible,setIsModalVisible] = useState(false)
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#faebd7', alignItems: 'center',  justifyContent: 'center'}}>
+        <Text>Home</Text>
+        <Button
+            title="Go to profile"
+            onPress={() =>
+                navigation.navigate('Profile', {name: 'Jane'})
+            }
+        />
+        {/* <Button title='Press' color={'midnightblue'} onPress={()=> { setIsModalVisible(true)}}></Button>
+        <Modal 
+          visible={isModalVisible}
+          animationType='slide'
+          presentationStyle='formSheet'>
+          <View style={{ flex: 1, backgroundColor: 'red', alignItems: 'center',  justifyContent: 'center'}}>
+            <Button title='Alert' color={'green'} onPress={()=> { Alert.alert('Alert!','Pick one',[
+              {
+                text: 'Left',
+                onPress: ()=> { console.log('left')}
+              },
+              {
+                text: 'Right',
+                onPress: ()=> { console.log('right')}
+              }
+            ])}}></Button>
+            <Button title='X' color={'midnightblue'} onPress={()=> { setIsModalVisible(false)}}></Button>
+          </View>
+        </Modal> */}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
